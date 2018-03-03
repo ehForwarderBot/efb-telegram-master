b3275b50f350ccdb0bd947c35c00201961841339
Eana Hufwe <ilove@1a23.com>
2018-03-04 09:56:49 +1100

    Add support to PTB 10.0.0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     ],
     install_requires=[
         "ehforwarderbot>=2.0.0b10",
-        "python-telegram-bot",
+        "python-telegram-bot>=10.0.0",
         "python-magic",
         "moviepy",
         "peewee",
