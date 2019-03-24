de5e115e7665e4e7c9c101094311b3913a0b0222
Eana Hufwe <ilove@1a23.com>
2019-03-24 20:26:08 +1100

    Fix python-telegram-bot version dependency issue

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     ],
     install_requires=[
         "ehforwarderbot>=2.0.0b15",
-        "python-telegram-bot>=10.0.0<12.0.0",
+        "python-telegram-bot>=10.0.0,<12.0.0",
         "python-magic",
         "moviepy",
         "peewee",
