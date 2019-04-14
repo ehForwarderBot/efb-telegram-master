0e3147dd340fca435e69bcb18e5d63e34e9298be
Eana Hufwe <ilove@1a23.com>
2019-04-14 22:04:18 +1000

    Escape for HTML formatting in `/extra` messages.

--- efb_telegram_master/commands.py
+++ efb_telegram_master/commands.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-
+import html
 from typing import Tuple, Dict, TYPE_CHECKING, List, Any, Union
 
 from telegram import Message, Update
@@ -130,9 +130,18 @@ class CommandsManager(LocaleMixin):
         msg = self._("<i>Click the link next to the name for usage.</i>\n")
         for n, i in enumerate(self.modules_list):
             if isinstance(i, EFBChannel):
-                msg += "\n\n<b>%s %s (%s)</b>" % (i.channel_emoji, i.channel_name, i.instance_id)
+                msg += "\n\n<b>{0} {1}".format(
+                    html.escape(i.channel_emoji),
+                    html.escape(i.channel_name))
+                if i.instance_id:
+                    msg += " ({})".format(html.escape(i.instance_id))
+                msg += "</b>"
+
             elif isinstance(i, EFBMiddleware):
-                msg += "\n\n<b>%s (%s)</b>" % (i.middleware_name, i.middleware_id)
+                msg += "\n\n<b>{} ({})</b>".format(
+                    html.escape(i.middleware_name),
+                    html.escape(i.middleware_id)
+                )
             else:
                 # This should not occur as modules_list shall
                 # consist of only EFBChannel and EFBMiddleware instances
@@ -141,7 +150,10 @@ class CommandsManager(LocaleMixin):
             if extra_fns:
                 for j in extra_fns:
                     fn_name = "/h_%s_%s" % (n, j)
-                    msg += "\n- <b>%s</b> %s" % (extra_fns[j].name, fn_name)
+                    msg += "\n- <b>{}</b> {}".format(
+                        html.escape(extra_fns[j].name),
+                        html.escape(fn_name)
+                    )
             else:
                 msg += "\n" + self._("No command found.")
         self.bot.send_message(update.effective_chat.id, msg, parse_mode="HTML")
@@ -158,11 +170,18 @@ class CommandsManager(LocaleMixin):
 
         command = getattr(channel, groupdict['command'])
 
-        msg = "<b>%s %s (%s)</b>" % (channel.channel_emoji, channel.module_name, channel.module_id)
+        msg = "<b>{0} {1}".format(
+            html.escape(channel.channel_emoji),
+            html.escape(channel.channel_name))
+        if channel.instance_id:
+            msg += " ({})".format(html.escape(channel.instance_id))
+        msg += "</b>"
 
         fn_name = "/%s_%s" % (groupdict['id'], groupdict['command'])
-        msg += "\n\n%s <b>(%s)</b>\n%s" % (
-            fn_name, command.name, command.desc.format(function_name=fn_name))
+        msg += "\n\n{} <b>({})</b>\n{}".format(
+            html.escape(fn_name),
+            html.escape(command.name),
+            html.escape(command.desc.format(function_name=fn_name)))
         self.bot.send_message(update.effective_chat.id, msg, parse_mode="HTML")
 
     def extra_call(self, bot, update, groupdict: Dict[str, str] = None):
