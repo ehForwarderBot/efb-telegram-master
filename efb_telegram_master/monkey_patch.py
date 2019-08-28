fe3e9da92f992fc79010362b498fc39d97412c5d
Eana Hufwe <ilove@1a23.com>
2019-08-28 17:02:49 +0800

    Resolves #68: Enclosed ptb_urllib monkey-patch in a try-except block to prevent code breaking when ptb is installed from Arch package repository.

--- efb_telegram_master/monkey_patch.py
+++ efb_telegram_master/monkey_patch.py
@@ -1,7 +1,17 @@
-from telegram.vendor.ptb_urllib3.urllib3 import fields as ptb_fields
-from urllib3.fields import format_header_param_html5
+def patch_ptb_urllib3():
+    # ptb.vendor.ptb_urllib3 replace RFC2231 field names with HTML5 field names
+    # Enclosed in a try-except block to prevent code breaking when ptb is installed
+    # from Arch package repository.
+    #
+    # See https://github.com/blueset/efb-telegram-master/issues/68.
+    try:
+        from telegram.vendor.ptb_urllib3.urllib3 import fields as ptb_fields
+        from urllib3.fields import format_header_param_html5
+
+        ptb_fields.format_header_param = format_header_param_html5
+    except ImportError:
+        pass
 
 
 def load_monkey_patches():
-    # ptb.vendor.urllib3 replace RFC2231 field names with HTML5 field names
-    ptb_fields.format_header_param = format_header_param_html5
+    patch_ptb_urllib3()
