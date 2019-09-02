c442ed70987fba7cb0bd157bf6daefbf2f1d0b70
Eana Hufwe <ilove@1a23.com>
2019-09-02 11:12:06 +0800

    Require minimum of python-telegram-bot v12.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     ],
     install_requires=[
         "ehforwarderbot>=2.0.0b18",
-        "python-telegram-bot>=12.0.0b1",
+        "python-telegram-bot>=12.0.0",
         "python-magic",
         "moviepy",
         "peewee",
