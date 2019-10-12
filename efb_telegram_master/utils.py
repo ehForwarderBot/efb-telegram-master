818c80e2745bb0a89fe93582dd84a2d45b228d27
Eana Hufwe <ilove@1a23.com>
2019-10-12 10:56:53 +0800

    Remove redundant comment

--- efb_telegram_master/utils.py
+++ efb_telegram_master/utils.py
@@ -143,7 +143,7 @@ def convert_tgs_to_gif(tgs_file: IO[bytes], gif_file: IO[bytes]):
         # heavy_strip(animation)
         # heavy_strip(animation)
         # animation.tgs_sanitize()
-        export_gif(animation, gif_file, skip_frames=1)  # , {"skip_frames": 5})
+        export_gif(animation, gif_file, skip_frames=1)
         return True
     except Exception:
         logging.exception("Error occurred while converting TGS to GIF.")
