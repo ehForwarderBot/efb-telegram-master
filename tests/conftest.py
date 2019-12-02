e69a6a4f4ddaddbec502dde6b943d981b02a4b41
Eana Hufwe <ilove@1a23.com>
2019-12-02 19:30:41 +0800

    test: migrate unittest to pytest framework

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,106 @@
 import pytest
+from pathlib import Path
+
+from ruamel.yaml import YAML
+
+import ehforwarderbot.utils
+import ehforwarderbot.coordinator
+
+from .mocks.slave import MockSlaveChannel
+from .bot import get_bot
+from efb_telegram_master import TelegramChannel
 
 
-@pytest.fixture
-def foo():
-    return None
+@pytest.fixture('session')
+def bot_info():
+    return get_bot()
+
+
+@pytest.fixture('session')
+def bot_token(bot_info):
+    return bot_info['token']
+
+
+@pytest.fixture('session')
+def bot_admins(bot_info):
+    return bot_info['admins']
+
+
+@pytest.fixture('session')
+def bot_admin(bot_admins):
+    return bot_admins[0]
+
+
+@pytest.fixture('session')
+def bot_groups(bot_info):
+    return bot_info['groups']
+
+
+@pytest.fixture('session')
+def bot_group(bot_groups):
+    return bot_groups[0]
+
+
+@pytest.fixture('session')
+def bot_channels(bot_info):
+    return bot_info['channels']
+
+
+def dump_config(file_path: Path, data):
+    """Dump YAML config to a file."""
+    yaml = YAML()
+    if not file_path.parent.exists():
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+    with file_path.open('w') as file:
+        yaml.dump(data, file)
+
+
+@pytest.fixture(scope='class')
+def monkey_class():
+    from _pytest.monkeypatch import MonkeyPatch
+    mpatch = MonkeyPatch()
+    yield mpatch
+    mpatch.undo()
+
+
+@pytest.fixture(scope="class")
+def coordinator(tmp_path_factory, monkey_class, bot_token, bot_admins) -> ehforwarderbot.coordinator:
+    """Loaded coordinator with ETM and mock modules"""
+    tmp_path = tmp_path_factory.mktemp("etm_test")
+    monkey_class.setenv("EFB_DATA_PATH", str(tmp_path))
+
+    # Framework configs
+    config_path = ehforwarderbot.utils.get_config_path()
+    dump_config(config_path, {
+        "master_channel": TelegramChannel.channel_id,
+        "slave_channels": ["tests.mocks.slave"],
+        "middlewares": []
+    })
+
+    # Load mock slave channel
+    ehforwarderbot.coordinator.add_channel(MockSlaveChannel())
+    # TODO: load another slave channel
+
+    channel_config_path = ehforwarderbot.utils.get_config_path(TelegramChannel.channel_id)
+    dump_config(channel_config_path, {
+        'token': bot_token,
+        'admins': bot_admins
+    })
+
+    ehforwarderbot.coordinator.add_channel(TelegramChannel())
+
+    yield ehforwarderbot.coordinator
+
+    ehforwarderbot.coordinator.master.stop_polling()
+    for i in ehforwarderbot.coordinator.slaves.values():
+        i.stop_polling()
+
+
+@pytest.fixture(scope="class")
+def channel(coordinator) -> TelegramChannel:
+    return coordinator.master
+
+
+@pytest.fixture(scope="class")
+def slave(coordinator) -> MockSlaveChannel:
+    return coordinator.slaves[MockSlaveChannel.channel_id]
--- tests/test_bot_manager.py
+++ tests/test_bot_manager.py
@@ -1,30 +1,14 @@
 import string
 import random
-from unittest.mock import patch, Mock
-
-import telegram
-
-from .base_test import StandardChannelTest
+from unittest.mock import patch
 
 
-class BotManagerTest(StandardChannelTest):
-    @classmethod
-    def setUpClass(cls):
-        # Monkey patch message senders
-        super().setUpClass()
+def test_prefix_suffix(channel, bot_admin):
+    message = channel.bot_manager.send_message(bot_admin, 'Message', prefix='Prefix', suffix='Suffix')
+    assert message.text == 'Prefix\nMessage\nSuffix'
 
-    @patch('telegram.Bot.send_message')
-    @patch('telegram.Bot.send_document')
-    def test_prefix_suffix(self, mock_send_document: Mock,  mock_send_message: Mock):
-        self.master.bot_manager.send_message('0', 'Message', prefix='Prefix', suffix='Suffix')
-        mock_send_message.assert_called_with('0', text='Prefix\nMessage\nSuffix')
-        mock_send_message.reset_mock()
-
-        msg_body = ''.join(random.choice(string.printable) for _ in range(100000))
-        self.master.bot_manager.send_message('0', msg_body, prefix='Prefix')
-        mock_send_message.assert_called()
-        self.assertEqual(mock_send_message.call_args[0][0], '0')
-        self.assertTrue(mock_send_message.call_args[1]['text'].startswith('Prefix\n' + msg_body[:50]))
-        self.assertTrue(len(mock_send_message.call_args[1]['text']) <=
-                        telegram.constants.MAX_MESSAGE_LENGTH)
+    msg_body = ''.join(random.choice(string.ascii_letters) for _ in range(100000))
+    with patch('telegram.Bot.send_document') as mock_send_document:
+        message = channel.bot_manager.send_message(bot_admin, msg_body, prefix='Prefix')
+        assert message.text.startswith('Prefix\n' + msg_body[:50])
         mock_send_document.assert_called()
