67f7051b02f09ca813dfd3907f30db3f7bed9431
Eana Hufwe <ilove@1a23.com>
2019-12-02 19:30:06 +0800

    build: minor syntax change

--- dodo.py
+++ dodo.py
@@ -14,8 +14,8 @@ DOIT_CONFIG = {
 
 
 def task_gettext():
-    pot = "./{package}/locale/{package}.pot".format(package=PACKAGE)
-    sources = glob.glob("./{package}/**/*.py".format(package=PACKAGE), recursive=True)
+    pot = f"./{PACKAGE}/locale/{PACKAGE}.pot"
+    sources = glob.glob(f"./{PACKAGE}/**/*.py", recursive=True)
     sources = [i for i in sources if "__version__.py" not in i]
     command = "xgettext --add-comments=TRANSLATORS --from-code=UTF-8 -o " + pot + " " + " ".join(sources)
     sources.append(README_BASE)
@@ -65,7 +65,7 @@ def task_msgfmt():
 
 
 def task_crowdin():
-    sources = glob.glob("./{package}/**/*.po".format(package=PACKAGE), recursive=True)
+    sources = glob.glob(f"./{PACKAGE}/**/*.po", recursive=True)
     sources.append("README.rst")
     return {
         "actions": ["crowdin upload sources"],
@@ -124,8 +124,8 @@ def task_bump_version():
 
 
 def task_mypy():
-    actions = ["mypy -p {} --ignore-missing-imports".format(PACKAGE)]
-    sources = glob.glob("./{package}/**/*.py".format(package=PACKAGE), recursive=True)
+    actions = [f"mypy -p {PACKAGE} --ignore-missing-imports"]
+    sources = glob.glob(f"./{PACKAGE}/**/*.py", recursive=True)
     sources = [i for i in sources if "__version__.py" not in i]
     return {
         "actions": actions,
@@ -134,11 +134,11 @@ def task_mypy():
 
 
 def task_test():
-    sources = glob.glob("./{package}/**/*.py".format(package=PACKAGE), recursive=True)
+    sources = glob.glob(f"./{PACKAGE}/**/*.py", recursive=True)
     sources = [i for i in sources if "__version__.py" not in i]
     return {
         "actions": [
-            "coverage run --source ./{} -m pytest".format(PACKAGE),
+            f"coverage run --source ./{PACKAGE} -m pytest",
             "coverage report"
         ],
         "file_dep": sources
@@ -148,7 +148,8 @@ def task_test():
 def task_build():
     return {
         "actions": [
-            "python setup.py sdist bdist_wheel"
+            "python setup.py sdist bdist_wheel",
+            f"rm -rf build {PACKAGE}.egg-info"
         ],
         "task_dep": ["test", "msgfmt", "bump_version"]
     }
@@ -159,7 +160,7 @@ def task_publish():
         __version__ = __import__(PACKAGE).__version__
         if 'dev' in __version__:
             raise ValueError(f"Cannot publish dev version ({__version__}).")
-        binarys = glob.glob("./dist/*{}*".format(__version__), recursive=True)
+        binarys = glob.glob(f"./dist/*{__version__}*", recursive=True)
         return " ".join(["twine", "upload"] + binarys)
     return {
         "actions": [CmdAction(get_twine_command)],
