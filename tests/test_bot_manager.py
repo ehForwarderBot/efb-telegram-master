76b5bcf3e614d320127cbc6594858fdf90b589c9
Eana Hufwe <ilove@1a23.com>
2019-12-14 17:07:34 +0800

    test: test for bot_manager module.

--- tests/test_bot_manager.py
+++ tests/test_bot_manager.py
@@ -1,14 +1,79 @@
 import string
 import random
+from typing import IO, Iterator
 from unittest.mock import patch
 
+import pytest
 
-def test_prefix_suffix(channel, bot_admin):
+
+def test_text_prefix_suffix(channel, bot_admin):
     message = channel.bot_manager.send_message(bot_admin, 'Message', prefix='Prefix', suffix='Suffix')
     assert message.text == 'Prefix\nMessage\nSuffix'
 
+    edited = channel.bot_manager.edit_message_text(
+        text="Edited text", prefix="Edited prefix", suffix="Edited suffix",
+        chat_id=message.chat_id, message_id=message.message_id)
+    assert edited.chat_id == message.chat_id
+    assert edited.message_id == message.message_id
+    assert edited.text == "Edited prefix\nEdited text\nEdited suffix"
+
+
+@pytest.fixture(scope='function')
+def image() -> Iterator[IO[bytes]]:
+    f = open('tests/mocks/image.png', 'rb')
+    yield f
+    f.close()
+
+
+def test_caption_prefix_suffix(channel, bot_admin, image):
+    message = channel.bot_manager.send_photo(bot_admin, image, caption='Message', prefix='Prefix', suffix='Suffix')
+    assert message.caption == 'Prefix\nMessage\nSuffix'
+
+    edited = channel.bot_manager.edit_message_caption(
+        caption="Edited text", prefix="Edited prefix", suffix="Edited suffix",
+        chat_id=message.chat_id, message_id=message.message_id)
+    assert edited.chat_id == message.chat_id
+    assert edited.message_id == message.message_id
+    assert edited.caption == "Edited prefix\nEdited text\nEdited suffix"
+
+
+def test_message_truncation(channel, bot_admin):
     msg_body = ''.join(random.choice(string.ascii_letters) for _ in range(100000))
     with patch('telegram.Bot.send_document') as mock_send_document:
         message = channel.bot_manager.send_message(bot_admin, msg_body, prefix='Prefix')
         assert message.text.startswith('Prefix\n' + msg_body[:50])
         mock_send_document.assert_called()
+
+
+def test_malformed_markdown_text(channel, bot_admin):
+    channel.bot_manager.send_message(
+        bot_admin,
+        "*some _strange_ styling* with [an *incomplete* link](https://example.com/this.is.a.(link",
+        parse_mode="markdown"
+    )
+
+
+def test_malformed_markdown_caption(channel, bot_admin, image):
+    channel.bot_manager.send_photo(
+        bot_admin,
+        image,
+        caption="*some _strange_ styling* with [an *incomplete* link](https://example.com/this.is.a.(link",
+        parse_mode="markdown"
+    )
+
+
+def test_malformed_html_text(channel, bot_admin):
+    channel.bot_manager.send_message(
+        bot_admin,
+        '<b>Bold and <i>italics</i> text</b> and <abbr title="unknown tag to Telegram">UTTT</abbr> and an <a href="https://example.com">incomplete link</a',
+        parse_mode="html"
+    )
+
+
+def test_malformed_html_caption(channel, bot_admin, image):
+    channel.bot_manager.send_photo(
+        bot_admin,
+        image,
+        caption='<b>Bold and <i>italics</i> text</b> and <abbr title="unknown tag to Telegram">UTTT</abbr> and an <a href="https://example.com">incomplete link</a',
+        parse_mode="html"
+    )
