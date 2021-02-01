ehForwarderBot/efb-telegram-master

commit 0a939d98637ed8efd25ab4eabcaf59c3bc082211

fix: partially fixed #108

efb_telegram_master/__init__.py
@@ -14,21 +14,22 @@ import telegram.error
 from PIL import Image, WebPImagePlugin
 from pkg_resources import resource_filename
 from ruamel.yaml import YAML
-from telegram import Message, Update
+from telegram import Update, Message
 from telegram.ext import CommandHandler, CallbackQueryHandler, CallbackContext, Filters
+from telegram.utils.types import HandlerArg
 
 import ehforwarderbot  # lgtm [py/import-and-import-from]
 from ehforwarderbot import Channel, coordinator
 from ehforwarderbot import utils as efb_utils
 from ehforwarderbot.channel import MasterChannel
-from ehforwarderbot.message import Message
+from ehforwarderbot.message import Message as EFBMessage
 from ehforwarderbot.chat import Chat
 from ehforwarderbot.status import Status
 from ehforwarderbot.constants import MsgType
 from ehforwarderbot.exceptions import EFBException, EFBOperationNotSupported, EFBChatNotFound, \
     EFBMessageReactionNotPossible
 from ehforwarderbot.status import ReactToMessage
-from ehforwarderbot.types import ModuleID, InstanceID, MessageID
+from ehforwarderbot.types import ModuleID, InstanceID, MessageID, ReactionName, ChatID
 from . import utils as etm_utils
 from .__version__ import __version__
 from .bot_manager import TelegramBotManager
@@ -41,7 +42,7 @@ from .master_message import MasterMessageProcessor
 from .message import ETMMsg
 from .rpc_utils import RPCUtilities
 from .slave_message import SlaveMessageProcessor
-from .utils import ExperimentalFlagsManager, EFBChannelChatIDStr
+from .utils import ExperimentalFlagsManager, EFBChannelChatIDStr, TelegramChatID, TelegramMessageID
 
 
 class TelegramChannel(MasterChannel):
@@ -196,12 +197,14 @@ class TelegramChannel(MasterChannel):
 
             self.config = data.copy()
 
-    def info(self, update: Update, context: CallbackContext):
+    def info(self, update: HandlerArg, context: CallbackContext):
         """
         Show info of the current telegram conversation.
         Triggered by `/info`.
         """
-        if update.message.chat.type != telegram.Chat.PRIVATE:  # Group message
+        assert isinstance(update, Update)
+        assert isinstance(update.effective_message, Message)
+        if update.effective_message.chat.type != telegram.Chat.PRIVATE:  # Group message
             msg = self.info_group(update)
         elif update.effective_message.forward_from_chat and \
                 update.effective_message.forward_from_chat.type == 'channel':  # Forwarded channel command.
@@ -209,7 +212,7 @@ class TelegramChannel(MasterChannel):
         else:  # Talking to the bot.
             msg = self.info_general()
 
-        update.message.reply_text(msg)
+        update.effective_message.reply_text(msg)
 
     def info_general(self):
         """Generate string for information of the current running EFB instance."""
@@ -312,10 +315,13 @@ class TelegramChannel(MasterChannel):
                     )
         return msg
 
-    def start(self, update: Update, context: CallbackContext):
+    def start(self, update: HandlerArg, context: CallbackContext):
         """
         Process bot command `/start`.
         """
+        assert isinstance(update, Update)
+        assert isinstance(update.effective_message, Message)
+        assert isinstance(update.effective_chat, Chat)
         if context.args:  # Group binding command
             if update.effective_message.chat.type != telegram.Chat.PRIVATE or \
                     (update.effective_message.forward_from_chat and
@@ -329,14 +335,16 @@ class TelegramChannel(MasterChannel):
                          "To learn more, please visit https://etm.1a23.studio .")
             self.bot_manager.send_message(update.effective_chat.id, txt)
 
-    def react(self, update: Update, context: CallbackContext):
+    def react(self, update: HandlerArg, context: CallbackContext):
         """React to a message."""
+        assert isinstance(update, Update)
+        assert isinstance(update.effective_message, Message)
         message: Message = update.effective_message
 
         reaction = None
         args = message.text and message.text.split(' ', 1)
         if args and len(args) > 1:
-            reaction = args[1]
+            reaction = ReactionName(args[1])
 
         if not message.reply_to_message:
             message.reply_html(self._("Reply to a message with this command and an emoji "
@@ -346,9 +354,9 @@ class TelegramChannel(MasterChannel):
                                       "from a message."))
             return
 
-        target: Message = update.message.reply_to_message
-        msg_log = self.db.get_msg_log(master_msg_id=etm_utils.message_id_to_str(chat_id=target.chat_id,
-                                                                                message_id=target.message_id))
+        target: Message = message.reply_to_message
+        msg_log = self.db.get_msg_log(master_msg_id=etm_utils.message_id_to_str(chat_id=TelegramChatID(target.chat_id),
+                                                                                message_id=TelegramMessageID(target.message_id)))
         if msg_log is None:
             message.reply_text(self._("The message you replied to is not recorded in ETM database. "
                                       "You cannot react to this message."))
@@ -397,7 +405,7 @@ class TelegramChannel(MasterChannel):
                                       "You cannot react to this message.").format(chat_uid))
             return
 
-        if reaction == "-":
+        if reaction == ReactionName("-"):
             reaction = None
 
         try:
@@ -413,7 +421,9 @@ class TelegramChannel(MasterChannel):
             message.reply_text(prompt)
             return
 
-    def help(self, update: Update, context: CallbackContext):
+    def help(self, update: HandlerArg, context: CallbackContext):
+        assert isinstance(update, Update)
+        assert isinstance(update.message, Message)
         txt = self._("EFB Telegram Master Channel\n"
                      "/link\n"
                      "    Link a remote chat to an empty Telegram group.\n"
@@ -436,7 +446,7 @@ class TelegramChannel(MasterChannel):
                      "    Remove the quoted message from its remote chat.\n"
                      "/help\n"
                      "    Print this command list.")
-        self.bot_manager.send_message(update.message.from_user.id, txt)
+        update.message.reply_text(txt)
 
     def poll(self):
         """
@@ -444,12 +454,14 @@ class TelegramChannel(MasterChannel):
         """
         self.bot_manager.polling()
 
-    def error(self, update: Update, context: CallbackContext):
+    def error(self, update: HandlerArg, context: CallbackContext):
         """
         Print error to console, and send error message to first admin.
         Triggered by python-telegram-bot error callback.
         """
-        error = context.error
+        assert isinstance(update, Update)
+        assert context.error
+        error: Exception = context.error
         if "make sure that only one bot instance is running" in str(error):
             now = time.time()
             # Warn the user only from the second time within ``CONFLICTION_TIMEOUT``
@@ -485,7 +497,7 @@ class TelegramChannel(MasterChannel):
             self.logger.error("Poor internet connection detected.\n"
                               "Number of network error occurred since last startup: %s\n%s\nUpdate: %s",
                               self.timeout_count, str(error), str(update))
-            if update is not None and isinstance(getattr(update, "message", None), telegram.Message):
+            if isinstance(update.message, Message):
                 update.message.reply_text(self._("This message is not processed due to poor internet environment "
                                                  "of the server.\n"
                                                  "<code>{code}</code>").format(code=html.escape(str(error))),
@@ -508,12 +520,13 @@ class TelegramChannel(MasterChannel):
                                               parse_mode="HTML")
         except telegram.error.ChatMigrated as e:
             new_id = e.new_chat_id
-            old_id = update.message.chat_id
+            assert isinstance(update.message, Message)
+            old_id = ChatID(str(update.message.chat_id))
             count = 0
             for i in self.db.get_chat_assoc(master_uid=etm_utils.chat_id_to_str(self.channel_id, old_id)):
                 self.logger.debug('Migrating slave chat %s from Telegram chat %s to %s.', i, old_id, new_id)
                 self.db.remove_chat_assoc(slave_uid=i)
-                self.db.add_chat_assoc(master_uid=etm_utils.chat_id_to_str(self.channel_id, new_id), slave_uid=i)
+                self.db.add_chat_assoc(master_uid=etm_utils.chat_id_to_str(self.channel_id, ChatID(str(new_id))), slave_uid=i)
                 count += 1
             self.bot_manager.send_message(
                 new_id, self.ngettext("Chat migration detected.\n"
@@ -539,14 +552,14 @@ class TelegramChannel(MasterChannel):
                 self.logger.exception('Unhandled telegram bot error!\n'
                                       'Update %s caused error %s. Exception', update, error)
 
-    def send_message(self, msg: Message) -> Message:
+    def send_message(self, msg: EFBMessage) -> EFBMessage:
         return self.slave_messages.send_message(msg)
 
     def send_status(self, status: Status):
         return self.slave_messages.send_status(status)
 
     def get_message_by_id(self, chat: Chat,
-                          msg_id: MessageID) -> Optional['Message']:
+                          msg_id: MessageID) -> Optional[EFBMessage]:
         origin_uid = etm_utils.chat_id_to_str(chat=chat)
         msg_log = self.db.get_msg_log(slave_origin_uid=origin_uid,
                                       slave_msg_id=msg_id)
@@ -556,7 +569,10 @@ class TelegramChannel(MasterChannel):
             # Message is not found.
             return None
 
-    def void_callback_handler(self, update: Update, context: CallbackContext):
+    def void_callback_handler(self, update: HandlerArg, context: CallbackContext):
+        assert isinstance(update, Update)
+        assert update.effective_message
+        assert update.callback_query
         self.bot_manager.answer_callback_query(update.callback_query.id,
                                                text=self._("This button does nothing."),
                                                message_id=update.effective_message.message_id,

efb_telegram_master/__version__.py
@@ -1,1 +1,1 @@
-__version__ = "2.2.2"
+__version__ = "2.2.3.dev1"

efb_telegram_master/bot_manager.py
@@ -12,6 +12,7 @@ import telegram.error
 from retrying import retry
 from telegram import Update, InputFile, User, File
 from telegram.ext import CallbackContext, Filters, MessageHandler, Updater, Dispatcher
+from telegram.utils.types import HandlerArg
 
 from .locale_handler import LocaleHandler
 from .locale_mixin import LocaleMixin
@@ -171,7 +172,9 @@ class TelegramBotManager(LocaleMixin):
             self.logger.debug("Webhook is set...")
 
         self.logger.debug("Checking connection to Telegram bot API...")
-        self.me: User = self.updater.bot.get_me()
+        me = self.updater.bot.get_me()
+        assert me, "Invalid bot credential provided."
+        self.me: User = me
         self.logger.debug("Connection to Telegram bot API is OK...")
         self.admins: List[int] = config['admins']
         self.dispatcher: Dispatcher = self.updater.dispatcher
@@ -476,7 +479,10 @@ class TelegramBotManager(LocaleMixin):
     def get_me(self, *args, **kwargs):
         return self.updater.bot.get_me(*args, **kwargs)
 
-    def session_expired(self, update: Update, context: CallbackContext):
+    def session_expired(self, update: HandlerArg, context: CallbackContext):
+        assert isinstance(update, Update)
+        assert update.effective_message
+        assert update.effective_chat
         if update.callback_query:
             update.callback_query.answer()
         self.edit_message_text(text=self._("Session expired. Please try again. (SE01)"),

efb_telegram_master/commands.py
@@ -6,6 +6,7 @@ from typing import Tuple, Dict, TYPE_CHECKING, List, Any, Union, Optional
 from telegram import Message, Update
 from telegram.ext import CommandHandler, ConversationHandler, CallbackQueryHandler, MessageHandler, CallbackContext
 from telegram.ext.filters import Filters
+from telegram.utils.types import HandlerArg
 
 from ehforwarderbot import coordinator, Channel, Middleware
 from ehforwarderbot.channel import SlaveChannel
@@ -74,7 +75,7 @@ class CommandsManager(LocaleMixin):
         self.command_conv.conversations[message_identifier] = Flags.COMMAND_PENDING
         self.msg_storage[message_identifier] = commands
 
-    def command_exec(self, update: Update, context: CallbackContext) -> Optional[int]:
+    def command_exec(self, update: HandlerArg, context: CallbackContext) -> Optional[int]:
         """
         Run a command from a command message.
         Triggered by callback message with status `Flags.COMMAND_PENDING`.
@@ -84,10 +85,16 @@ class CommandsManager(LocaleMixin):
         Returns:
             The next state
         """
+        assert isinstance(update, Update)
+        assert update.effective_chat
+        assert update.effective_message
+        assert update.callback_query
 
         chat_id = update.effective_chat.id
         message_id = update.effective_message.message_id
         callback = update.callback_query.data
+
+        assert callback
 
         index = (chat_id, message_id)
 
@@ -104,10 +111,10 @@ class CommandsManager(LocaleMixin):
             update.callback_query.answer()
             return ConversationHandler.END
 
-        callback = int(callback)
+        callback_idx = int(callback)
         command_storage = self.msg_storage[index]
         module = command_storage.module
-        command = command_storage.commands[callback]
+        command = command_storage.commands[callback_idx]
         prefix = command_storage.prefix
 
         self.logger.debug("[%s.%s] Command execution callback is valid. Command storage item: %s", chat_id, message_id, command_storage)
@@ -143,11 +150,14 @@ class CommandsManager(LocaleMixin):
         )
         return ConversationHandler.END
 
-    def extra_listing(self, update: Update, context: CallbackContext):
+    def extra_listing(self, update: HandlerArg, context: CallbackContext):
         """
         Show list of additional features and their usage.
         Triggered by `/extra`.
         """
+        assert isinstance(update, Update)
+        assert update.effective_chat
+
         msg = self._("<i>Click the link next to the name for usage.</i>\n")
         for idx, i in enumerate(self.modules_list):
             if isinstance(i, Channel):
@@ -180,7 +190,11 @@ class CommandsManager(LocaleMixin):
                 msg += "\n" + self._("No command found.")
         self.bot.send_message(update.effective_chat.id, msg, parse_mode="HTML")
 
-    def extra_usage(self, update: Update, context: CallbackContext):
+    def extra_usage(self, update: HandlerArg, context: CallbackContext):
+        assert context.match
+        assert isinstance(update, Update)
+        assert update.effective_chat
+
         groupdict = context.match.groupdict()
         if int(groupdict['id']) >= len(self.modules_list):
             return self.bot.reply_error(update, self._("Invalid module ID. (XC03)"))
@@ -207,10 +221,14 @@ class CommandsManager(LocaleMixin):
             html.escape(command.desc.format(function_name=fn_name)))
         self.bot.send_message(update.effective_chat.id, msg, parse_mode="HTML")
 
-    def extra_call(self, update: Update, context: CallbackContext):
+    def extra_call(self, update: HandlerArg, context: CallbackContext):
         """
         Invoke an additional feature from slave channel.
         """
+        assert context.match
+        assert isinstance(update, Update)
+        assert update.message
+
         groupdict = context.match.groupdict()
         if int(groupdict['id']) >= len(coordinator.slaves):
             return self.bot.reply_error(update, self._("Invalid module ID. (XC01)"))
@@ -231,6 +249,7 @@ class CommandsManager(LocaleMixin):
         msg = self.bot.send_message(update.message.chat.id,
                                     prefix=header, text=self._("Please wait..."))
 
+        assert update.message.text
         result = functions[ExtraCommandName(groupdict['command'])](
             " ".join(update.message.text.split(' ', 1)[1:]))
 

efb_telegram_master/db.py
@@ -24,7 +24,7 @@ from .chat_object_cache import ChatObjectCacheManager
 from .message import ETMMsg
 from .msg_type import TGMsgType
 from .utils import TelegramChatID, EFBChannelChatIDStr, TgChatMsgIDStr, message_id_to_str, \
-    chat_id_to_str, OldMsgID, chat_id_str_to_id
+    chat_id_to_str, OldMsgID, chat_id_str_to_id, TelegramMessageID
 
 if TYPE_CHECKING:
     from . import TelegramChannel
@@ -408,7 +408,7 @@ class DatabaseManager:
                                   master_message: Message,
                                   old_message_id: Optional[OldMsgID] = None):
         """Add or update a message into the database."""
-        master_msg_id = message_id_to_str(master_message.chat_id, master_message.message_id)
+        master_msg_id = message_id_to_str(TelegramChatID(master_message.chat_id), TelegramMessageID(master_message.message_id))
         master_msg_id_alt = None
         self.logger.debug("[%s] Received message logging request of %s", master_msg_id, msg.uid)
 

efb_telegram_master/locale_handler.py
@@ -8,6 +8,7 @@ from typing import TYPE_CHECKING
 from language_tags import tags
 from telegram.ext.handler import Handler
 from telegram import Update
+from telegram.utils.types import HandlerArg
 
 if TYPE_CHECKING:
     from . import TelegramChannel
@@ -34,8 +35,10 @@ class LocaleHandler(Handler):
         self.channel = channel
         self.auto_locale = self.channel.flag('auto_locale')
 
-    def check_update(self, update: Update):
+    def check_update(self, update: HandlerArg):
         if not self.auto_locale:
+            return False
+        if not isinstance(update, Update):
             return False
         if not update.effective_user or not update.effective_user.language_code:
             return

commit 33765780c555f3987a8926fc6af0368003ba38af

fix: wrong name reference introduced by 0a939d9

efb_telegram_master/__init__.py
@@ -320,8 +320,8 @@ class TelegramChannel(MasterChannel):
         Process bot command `/start`.
         """
         assert isinstance(update, Update)
-        assert isinstance(update.effective_message, Message)
-        assert isinstance(update.effective_chat, Chat)
+        assert isinstance(update.effective_message, telegram.Message)
+        assert isinstance(update.effective_chat, telegram.Chat)
         if context.args:  # Group binding command
             if update.effective_message.chat.type != telegram.Chat.PRIVATE or \
                     (update.effective_message.forward_from_chat and

efb_telegram_master/__version__.py
@@ -1,1 +1,1 @@
-__version__ = "2.2.3.dev6"
+__version__ = "2.2.3.dev7"