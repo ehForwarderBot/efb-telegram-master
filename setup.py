968c6baae67870b96a1020b525e215374eb856b5
Eana Hufwe <ilove@1a23.com>
2021-04-11 09:58:27 -0700

    fix: updated to follow ``python-telegram-bot`` 13.4 API changes

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     tests_require=tests_require,
     install_requires=[
         "ehforwarderbot>=2.0.0",
-        "python-telegram-bot~=13.2",
+        "python-telegram-bot~=13.4",
         "python-magic",
         "ffmpeg-python",
         "peewee",
