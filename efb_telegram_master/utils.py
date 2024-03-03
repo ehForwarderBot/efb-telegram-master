2a01b0345e767981680a292c08cfb0a2ca21e352
Ovler <ovlertheyoung@gmail.com>
2024-03-04 02:50:46 +0800

    Refactor GIF compression logic to make code simple

--- efb_telegram_master/utils.py
+++ efb_telegram_master/utils.py
@@ -296,63 +296,41 @@ else:
         stream = ffmpeg.input(file.name)
         # 检查视频编码类型是否为VP9
         if metadata['streams'][0]['codec_name'] == 'vp9':
-            stream = ffmpeg.input(file.name, vcodec='libvpx-vp9')
+            stream = ffmpeg.input(file.name, vcodec='libvpx-vp9') # 只有这个能保持透明背景
         if channel_id.startswith("blueset.wechat"):
             # Workaround: Compress GIF for slave channel `blueset.wechat`
             # TODO: Move this logic to `blueset.wechat` in the future
             if metadata.get('fps', 0) > 12:
-                stream = stream.filter("fps", 12, round='up')
+                stream = stream.filter("fps", 12, round='up') # 限制帧率
             if metadata.get('width', 0) > 600:
-                stream = stream.filter("scale", 600, -2, flags="lanczos")
-        split = (
-            stream
-            .split()
-        )
-        stream_paletteuse = (
-            ffmpeg
-            .filter(
-                [
-                    split[0],
-                    split[1]
-                    .filter(
-                        filter_name='palettegen', 
-                        reserve_transparent='on',
-                    )
-                ],
-                filter_name='paletteuse',
+                metadata['streams'][0]['width'] = 600 # 限制宽度,更宽的不会再出现
+        scales = [metadata['streams'][0]['width'], 600, 512, 480, 400, 360, 300, 256, 250, 200, 150, 100]
+        scales = [scale for scale in scales if scale <= metadata['streams'][0]['width']]
+        scales = sorted(scales, reverse=True)
+        for scale in scales:
+            stream_scale = stream.filter("scale", scale, -2, flags="lanczos") # scale要放在split和palettegen之前，否则透明会变成黑色或绿色
+            split = (
+                stream_scale
+                .split()
             )
-        )
-        stream_paletteuse.output(gif_file.name).overwrite_output().run()
-        # get the gif file size
-        new_file_size = os.path.getsize(gif_file.name)
-        if new_file_size > 1024 * 1024:
-            scales = [600, 512, 480, 400, 360, 300, 256, 250, 200, 150, 100]
-            scales = [scale for scale in scales if scale < metadata['streams'][0]['width']]
-            if channel_id.startswith("blueset.wechat"):
-                for scale in scales:
-                    stream_scale = stream.filter("scale", scale, -2, flags="lanczos")
-                    split = (
-                        stream_scale
-                        .split()
-                    )
-                    stream_paletteuse = (
-                        ffmpeg
+            stream_paletteuse = (
+                ffmpeg
+                .filter(
+                    [
+                        split[0],
+                        split[1]
                         .filter(
-                            [
-                                split[0],
-                                split[1]
-                                .filter(
-                                    filter_name='palettegen', 
-                                    reserve_transparent='on',
-                                )
-                            ],
-                            filter_name='paletteuse',
+                            filter_name='palettegen', 
+                            reserve_transparent='on',
                         )
-                    )
-                    stream_paletteuse.output(gif_file.name, fs=1400000).overwrite_output().run() 
-                    new_file_size = os.path.getsize(gif_file.name)
-                    if new_file_size < 1024 * 1024:
-                        break
+                    ],
+                    filter_name='paletteuse',
+                )
+            )
+            stream_paletteuse.output(gif_file.name, fs=1400000).overwrite_output().run() 
+            new_file_size = os.path.getsize(gif_file.name)
+            if new_file_size < 1024 * 1024 or not channel_id.startswith("blueset.wechat"):
+                break
         file.close()
         gif_file.seek(0)
         return gif_file
